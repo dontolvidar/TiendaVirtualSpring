b38fc07299c2e96f429612e823ba656646473a7d
Jorge <joradiazf@gmail.com>
2021-09-24 09:10:20 -0500

    Inicio del proyecto

---
+++ src/main/java/com/uelbosque/tiendavirtual/TiendaVirtualSpringApplication.java
@@ -0,0 +1,13 @@
+package com.uelbosque.tiendavirtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaVirtualSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaVirtualSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uelbosque/tiendavirtual/TiendaVirtualSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uelbosque.tiendavirtual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaVirtualSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
